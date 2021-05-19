405d6fd3829807f9df16e2d09c997b46e78dfc72
sankalp 123-w <mjha9717386401@gmail.com>
2021-05-19 12:16:05 +0530

    intial

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const port = process.env.PORT||3000;
+const ejs = require("ejs");
+const path = require("path");
+const app = express();
+app.set("views",path.join(__dirname,"resources/views"));
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+res.render("index");
+});
+
+app.listen(port,()=>{
+	console.log(`listening to${port}`);
+});
