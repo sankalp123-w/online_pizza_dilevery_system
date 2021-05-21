0d96e107d2ccc4ac68a9fec0395a774ba5684970
sankalp 123-w <mjha9717386401@gmail.com>
2021-05-21 22:32:05 +0530

    login and register page

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require("express");
 const port = process.env.PORT||3000;
 const ejs = require("ejs");
 const path = require("path");
+const layouts = require("express-ejs-layouts");
 const app = express();
 
+
 app.use(express.static('public'));
+app.use(layouts);
 app.set("views",path.join(__dirname,"resources/views"));
 app.set("view engine","ejs");
+
+
 app.get("/",(req,res)=>{
 res.render("index");
 });
+app.get("/cart",(req,res)=>{
+res.render("coustomer/cart");
+});
+app.get("/login",(req,res)=>{
+res.render("auth/login");
+});
+app.get("/register",(req,res)=>{
+res.render("auth/register");
+});
+
 
 app.listen(port,()=>{
 	console.log(`listening to${port}`);
