89bacf26cd95960f8919e8cffa4dc105ccaaf3af
sankalp 123-w <mjha9717386401@gmail.com>
2021-05-21 13:02:58 +0530

    home page added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const port = process.env.PORT||3000;
 const ejs = require("ejs");
 const path = require("path");
 const app = express();
+
+app.use(express.static('public'));
 app.set("views",path.join(__dirname,"resources/views"));
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
