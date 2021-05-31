659e3590a1fd95d8af5ced03ada55ee9842fc02d
sankalp 123-w <mjha9717386401@gmail.com>
2021-05-31 17:53:04 +0530

    tryig atlas cluster

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ secret:process.env.SECRET_KEY,
 resave:false,
 saveUninitialized:true,
 store: MongoDbConnect.create({
-    mongoUrl: 'mongodb://localhost:27017/mynewapp22',
+    mongoUrl: process.env.MONGO_CONNECTION_URL||'mongodb://localhost:27017/mynewapp22',
    
   }),
  cookie:{maxAge:1000*60*60*24}, 
