94bd2c7afb2d5ca1cda44f3fbeb39899e974bd10
sankalp 123-w <mjha9717386401@gmail.com>
2021-05-31 17:07:46 +0530

    trying to connect mongodb

--- app/db/connc.js
+++ app/db/connc.js
@@ -1,7 +1,7 @@
 //C:\Program Files\MongoDB\Server\3.2\bin
 
 const mongoose=require("mongoose");
-mongoose.connect(process.env.MONGO_CONNECTION_URL,{useCreateIndex:true,useUnifiedTopology:true,useNewUrlParser:true,useFindAndModify:false});
+mongoose.connect(process.env.MONGO_CONNECTION_URL||"mongodb://localhost:27017/mynewapp22",{useCreateIndex:true,useUnifiedTopology:true,useNewUrlParser:true,useFindAndModify:false});
 const connection = mongoose.connection;
 connection.once('open',()=>{
 	console.log("db connected")
